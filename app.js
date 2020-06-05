6d92d78bf8917938c9fa2509569b8278265bb0df
harshay <harshaykr@hotmail.com>
2020-06-05 11:59:47 +0100

    update

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('hello world'); 
+/*************************************************************************************************
+ * Harshay
+ * Full Stack Javascript
+ * Techdegree
+ * Treehouse
+ * Project 6 : Static Node.js & Express Site
+ * Grade : Exceeds Expectation
+ ************************************************************************************************/
+
+const express = require('express'); 
+
+//create express application 
+const app = express(); 
+
+//Optionally - the path module which can be used when setting the absolute path in the express.static function.
+
+//set up templating engine  
+app.set('view engine','pug'); 
+
+//index
+app.get('/views/',(req,res) => { 
+
+    res.render('index'); 
+
+});
+
+//set up development server
+app.listen(3000);
+
+
+
+
