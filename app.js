96bfeaf8c5b687c61968d8a6765bebba3483719d
harshay <harshaykr@hotmail.com>
2020-06-05 16:28:14 +0100

    update

--- app.js
+++ app.js
@@ -7,24 +7,41 @@
  * Grade : Exceeds Expectation
  ************************************************************************************************/
 
+//notes
+
+//locals meaning reference
+//https://stackoverflow.com/questions/32688857/what-is-meaning-of-locals-in-res-render-node-js
+//https://gist.github.com/joepie91/c0069ab0e0da40cc7b54b8c2203befe1
+
+ ///////////////////////////////////////////////////////////////////////////////////////////////////
+
 const express = require('express'); 
 
 //create express application 
 const app = express();
 
 //import json data file 
-//const data = require('data.json').projects;
-
+// use './' to move up/outside the node_modules folder as the file is in the root
+const data = require('./data.json');
 
 //Optionally - the path module which can be used when setting the absolute path in the express.static function.
 
 //set up templating engine  
-app.set('view engine','pug'); 
+app.set('view engine','pug');
+
+//use a static route and the express.static method to serve the static files located in the public folder
 
 //index
-app.get('/views',(req,res) => { 
+app.get('/',(req,res) => { 
 
     res.render('index'); 
+
+});
+
+//index
+app.get('/',(req,res) => { 
+
+    res.render('about'); 
 
 });
 
