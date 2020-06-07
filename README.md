1e645d9a61117964f9c7b7fe9659eeda109cdb92
harshay <harshaykr@hotmail.com>
2020-06-07 14:22:07 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # fs_js_techdegree_project_6
  
+changes for exceeds grade:
+
+- changed background colour to lightgrey
+- changed font colour to darkgrey
+- changed font style to calibri
--- app.js
+++ app.js
@@ -6,26 +6,11 @@
  * Project 6 : Static Node.js & Express Site
  * Grade : Exceeds Expectation
  ************************************************************************************************/
-
 //notes
 
-//locals meaning reference
-//https://stackoverflow.com/questions/32688857/what-is-meaning-of-locals-in-res-render-node-js
-//https://gist.github.com/joepie91/c0069ab0e0da40cc7b54b8c2203befe1
 
-//requiring modules and defining path 
-//https://www.freecodecamp.org/news/requiring-modules-in-node-js-everything-you-need-to-know-e7fbd119be8/
-
-//res locals explained in express
-//https://www.youtube.com/watch?v=IuqhkjkcXbo
-
-//pug loop explained
-//https://www.youtube.com/watch?v=4ciMkWOAGRY 
-
-//pug cheatsheet
-//https://devhints.io/pug
-
- ///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//basic app set up
 
  //import express module
 const express = require('express'); 
@@ -43,20 +28,34 @@ app.set('view engine','pug');
 //use a static route and the express.static method to serve the static files located in the public folder
 app.use('/static', express.static('public'));
 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 //skills array
 const about_skills = ['HTML','CSS','Javascript','Python','Machine Learning'];
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//set up routes
+
 
 //render and return the ; index page
 app.get('/',(req,res) => { 
 
     //set variables
     res.locals.index_heading = "Harshay's Developer Portfolio";
-    res.locals.data_obj_array = data.projects; 
+    res.locals.data_obj_array = data.projects;   
+
+    res.locals.layout_name  = 'Harshay';
+    res.locals.layout_description  = 'Full Stack Javascript Web Developer';
     res.locals.portfolio_description = "Web Development & Data Science Projects";
 
     res.render('index'); 
 
 });
+
+
+//////////////////////////////////////////
 
 //render and return the ; about page 
 app.get('/about',(req,res) => { 
@@ -75,21 +74,42 @@ app.get('/about',(req,res) => {
 
 });
 
+
+//////////////////////////////////////////
+
+//render and return the ; project page
+app.get('/project/:id',(req,res) => {  
+
+    //set variables
+    res.locals.data_obj_array = data.projects; 
+    res.locals.project_title = res.locals.data_obj_array[req.params.id].project_name;   
+    res.locals.project_description = res.locals.data_obj_array[req.params.id].project_name;
+    res.locals.project_description = res.locals.data_obj_array[req.params.id].project_name;  
+    res.locals.technologies = res.locals.data_obj_array[req.params.id].technologies;  
+    res.locals.project_images = res.locals.data_obj_array[req.params.id].img_urls;  
+    res.locals.project_github_link = res.locals.data_obj_array[req.params.id].github_link;  
+
+    res.render('project'); 
+
+});
+
+
+//////////////////////////////////////////
+
+//render and return the : error page  - when user enters a url that does not exist 
+app.use((req,res,next) => { 
+
+    const err = new Error('Oops! This page does not exist');
+    res.locals.error = err;
+    err.status = 404;
+    res.render('error');
+
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 //set up development server
 app.listen(3000);
 
 
-/*
-
-//img, link tags loop code
-
-      //- Use the Pug `each` iterator to create a project img, title and link in the markup for each of the projects you are showcasing.  
-      
-      //- Use a combination of interpolation and template literals as needed to dynamically generate the the routes in the href, as well as the img's src and the projects title.
-      
-      //- Here's what the Pug for each project example should look like. 
-.cell
-        a(href='#')
-          img.thumbnail(src='https://placehold.it/550x550')
-          h5 Title
-*/
+///////////////////////////////////////////////////////////////////////////////////////////////////
